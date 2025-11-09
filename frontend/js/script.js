654edeb20b4219bff43dc38a30363bd5913b32a5
Anirban Sarkar <sanirban2006@gmail.com>
2025-11-09 19:31:35 +0530

    frontend merged

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,18 +1,3 @@
-//chiabrata works
-// var swiper = new Swiper(".mySwiper", {
-//   slidesPerView: 3,       // show 3 cards at a time
-//   spaceBetween: 30,       // gap between cards
-//   grabCursor: true,
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-// });
-
 const swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   spaceBetween: 30,
@@ -64,6 +49,7 @@ gsap.from("#text p", {
   delay: 0.3,
   ease: "power3.out"
 });
+
 // login and register js
 
 // Intro animation then show auth
